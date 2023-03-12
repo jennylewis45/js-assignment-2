76092199d2208ac377c210cd507b8cac1f1449fa
jennylewis45 <121876323+jennylewis45@users.noreply.github.com>
2023-03-13 00:00:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+
+// var num=10;
+// document.write("the value is 10")
+// document.write("<br>")
+
+
+// document.write(num+1)
+// document.write("<br>")
+// document.write("now the number is 11")
+
+
+
+// document.write("<br>")
+
+// var newNum=11
+
+// document.write(newNum+1)
+// document.write("<br>")
+// document.write("now the number is 12")
+// document.write("<br>")
+
+
+// var Newnum=12
+// document.write(Newnum-1)
+// document.write("<br>")
+// document.write("now the number is 11")
+
+// document.write("<br>")
+// var Newnum1=11
+// document.write(Newnum1-1)
+// document.write("<br>")
+// document.write("now the number is 10")
+
+
+
+
+// var name=prompt("enter your name")
+// alert("Welcome" + "" +"" + name)
+
+
+
+
+// var num = prompt("Enter Number", "0") //prompt user to enter the number
+// var num = parseInt(num); //parse the num to number
+// var i = 0;
+// document.write('<table border="1" cellspacing="0">');
+// for (i = 1; i <= 10; i++) {
+//   document.write("<tr><td>" + 5 + " x " + i + " = " + 5 * i + "</td></tr>");
+// }
+
+
+
+
+
+// var name=prompt("enter first subject name")
+// var name=prompt("enter second subject name")
+// var name=prompt("enter third subject name")
+
+
+// var mt=prompt("enter the marks of maths out of 100")
+// var eng=prompt("enter the marks of english out of 100")
+// var sci=prompt("enter the marks of science out of 100")
+
+
+// var total=Number(mt)+Number(eng)+(sci)
+// var avg=Number(total)/3
+// var pc=Number(total)*100/300
+
+
+// document.write("total marks of sub=" +total)
+// document.write("<br> Average=" +avg)
+// document.write("<br> Percentage=" +pc)
+
